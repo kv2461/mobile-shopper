9785bd34c1643f323df6e5d287095b02b9db3a54
Kevin Vicencio <kevinarnoldcode@gmail.com>
2022-08-19 16:28:12 -0700

    experimented with state

--- mobile-shopper/App.js
+++ mobile-shopper/App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 //View is div - test
 export default function App() {
+  const [counter, setCounter] = useState(0)
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>{counter !== 10 ? counter : 'I love you'}</Text>
+      <Button
+        style={{fontSize: 20, color: 'green'}}
+        styleDisabled={{color: 'red'}}
+        onPress={() => setCounter(counter+1)}
+        title="Press Me"
+      >
+        Press Me
+      </Button>
       <StatusBar style="auto" />
     </View>
   );
